bdfee0d901c5c88c75b927d936734b4d22b41aaf
sachinmona98 <sachinravi1998@gmail.com>
2022-01-11 22:29:20 +0530

    Create PrintAnagramsTogether.cpp
    Given an array of strings, return all groups of strings that are anagrams.

---
+++ GoldmanSachs/PrintAnagramsTogether.cpp
@@ -0,0 +1,19 @@
+ vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        
+        unordered_map<string, vector<string>> mp;
+        for(int i = 0; i < string_list.size(); i++)
+        {
+            string key = string_list[i];
+            sort(key.begin(), key.end());
+            mp[key].push_back(string_list[i]);
+            
+        }
+        vector<vector<string>> ans;
+        for(auto i : mp)
+        {
+            ans.push_back(i.second);
+        }
+        return ans;
+        
+    }
